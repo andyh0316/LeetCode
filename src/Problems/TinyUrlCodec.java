73433690307bfb65a4d7d3009ee99bdabcc7a41c
andyh0316 <andyhong0316@yahoo.com>
2021-09-15 19:14:29 -0700

    update

--- src/Problems/TinyUrlCodec.java
+++ src/Problems/TinyUrlCodec.java
@@ -4,16 +4,22 @@ import java.util.HashMap;
 
 public class TinyUrlCodec {
     String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //String chars = "ABC";
+    int charsLength = chars.length();
     HashMap<String, String> map = new HashMap<>();
-    int count = 1000000000;
+    int count = 1;
 
+    // The concept here is we take all possible 62 characters, use modular and division to produce unique combination
+    // of characters based on counter. For example:
+    // 0 = A, 1 = B, 2 = C, 3 = AA, 4 = BA, 5 = CA, 6 = AAA, 7 = AAB, 8 = AAC
     public String getString() {
         int c = count;
         StringBuilder sb = new StringBuilder();
+
         while (c > 0) {
             c--;
-            sb.append(chars.charAt(c % 62));
-            c /= 62;
+            sb.append(chars.charAt(c % charsLength));
+            c /= charsLength;
         }
         return sb.toString();
     }
